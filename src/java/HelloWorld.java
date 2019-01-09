1c6a2b77534be229ebbb71c3ff5d1e4c64f4abf5
Statham Li <18600766695@163.com>
2019-01-09 20:01:12 +0800

    first commit code

---
+++ src/java/HelloWorld.java
@@ -0,0 +1,10 @@
+package test_package;
+
+/**
+ *  Created  a  new java on 2019/01/09
+ * */
+public class HelloWorld {
+    public  static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
